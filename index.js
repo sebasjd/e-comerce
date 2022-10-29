934e7a4f39542e45f478ade4cbaaea2a9ad5cc87
sebasjd <d.jsebastian@hotmail.com>
2022-10-29 01:35:15 -0300

    Render Hpage whit infinite scroll. (bug en mobile)

--- index.js
+++ index.js
@@ -1,25 +1,24 @@
 const articulesContainer = document.getElementById("articulesContainer");
 
 
-//renderizar productos de forma aleatoria
-
+// Render product on the home page
 let renderHomePage = () => {
-    const articules = document.createElement('section');
-    articulesContainer.append(articules);
-    articules.classList.add('articules');
-    const aleatoryProduct = () => {
-        for (let i = 0; i <= arrayProducts.length; i++) {
-            const aleatoryProduct = arrayProducts[Math.floor(Math.random() * arrayProducts.length)];
-            return aleatoryProduct
+    for (let i = 0; i <= 3; i++) {
+        const articules = document.createElement('section');
+        articulesContainer.append(articules);
+        articules.classList.add('articules');
+        const aleatoryProduct = () => {
+            for (let i = 0; i <= arrayProducts.length; i++) {
+                const aleatoryProduct = arrayProducts[Math.floor(Math.random() * arrayProducts.length)];
+                return aleatoryProduct
+            }
         }
-    }
-
-    const renderProduct = (producto) => {
-        const productImg = producto.productImg;
-        const name = producto.name;
-        const price = producto.price;
-        const productCard =
-            `<section class="articles">
+        const renderProduct = (producto) => {
+            const productImg = producto.productImg;
+            const name = producto.name;
+            const price = producto.price;
+            const productCard =
+                `<section class="articles">
         <div class="product">
             <img src="${productImg}" alt="">
             <div class="info">
@@ -32,11 +31,48 @@ let renderHomePage = () => {
             <button>Add to cart</button>
         </div>
     </section>`
-        articules.innerHTML = productCard;
+            articules.innerHTML = productCard;
+        }
+        renderProduct(aleatoryProduct())
     }
-    renderProduct(aleatoryProduct())
 }
-renderHomePage()
+document.addEventListener("DOMContentLoaded", renderHomePage())
+    // Render product on the home page
 
 
-// console.log(aleatoryArray);
+//Infinite scroll on home page
+
+// Show loader
+window.addEventListener("scroll", () => {
+    const {
+        scrollTop,
+        scrollHeight,
+        clientHeight
+    } = document.documentElement;
+    if (scrollTop + clientHeight >= scrollHeight) {
+
+        console.log("Final de la página");
+    }
+
+
+
+})
+
+// Infinite scroll on home page
+// show new page and remove loader
+window.addEventListener("scroll", () => {
+        const {
+            scrollTop,
+            scrollHeight,
+            clientHeight
+        } = document.documentElement;
+        let algo = () => {
+            if (scrollTop + clientHeight >= scrollHeight) {
+                renderHomePage();
+                console.log("Cargada la nueva página");
+            }
+        }
+        setTimeout(algo, 2000)
+
+    })
+    // Infinite scroll on home page
